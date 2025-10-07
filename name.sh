a3d8e9067752bb349b45d902368c91ff11085c4b
Redefined <freakgaming2009@gmail.com>
2025-10-07 19:26:38 +0530

    Update name.sh

--- name.sh
+++ name.sh
@@ -9,12 +9,24 @@ RESET="\e[0m"
 # Credits
 echo -e "${CYAN}Script made by NighT | Discord: Nightt.js${RESET}"
 
-# Prompt for new hostname
+# Ask for new hostname
 read -rp "$(echo -e "${YELLOW}Enter the new hostname: ${RESET}")" new_hostname
 
-# Set the hostname using hostnamectl
-sudo hostnamectl set-hostname "$new_hostname"
-echo -e "${GREEN}Hostname successfully changed to: ${new_hostname}${RESET}"
-
-# Show current hostname immediately
-echo -e "${CYAN}Current hostname is: $(hostname)${RESET}"
+# Ask whether to update now
+echo -e "${CYAN}Do you want to apply the new hostname now?${RESET}"
+select choice in "Yes" "No"; do
+    case $choice in
+        Yes)
+            sudo hostnamectl set-hostname "$new_hostname"
+            echo -e "${GREEN}Hostname applied immediately: ${new_hostname}${RESET}"
+            break
+            ;;
+        No)
+            echo -e "${YELLOW}Hostname will take effect on next login/reconnect: ${new_hostname}${RESET}"
+            break
+            ;;
+        *)
+            echo "Invalid choice. Please choose 1 or 2."
+            ;;
+    esac
+done
